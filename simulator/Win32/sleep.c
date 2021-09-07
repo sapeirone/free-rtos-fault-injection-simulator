c6b888ea72e2757d6a3bb17055dd0d821b1c0ef0
Marzio <marziovallero@gmail.com>
2021-09-07 16:25:46 +0200

    Add Win32 sleepNanoseconds

--- simulator/Win32/sleep.c
+++ simulator/Win32/sleep.c
@@ -1,6 +1,19 @@
 #include "sleep.h"
+#include "windows.h"
 
 void sleepNanoseconds(unsigned long ns)
 {
-    (void)ns;
+    LONGLONG llns = (LONGLONG) ns / 100LL;
+	HANDLE timer;
+	LARGE_INTEGER li;
+	if(!(timer = CreateWaitableTimer(NULL, TRUE, NULL)))
+		return FALSE;
+	li.QuadPart = -llns;
+	if(!SetWaitableTimer(timer, &li, 0, NULL, NULL, FALSE)){
+		CloseHandle(timer);
+		return FALSE;
+	}
+	WaitForSingleObject(timer, INFINITE);
+	CloseHandle(timer);
+	return;
 }
