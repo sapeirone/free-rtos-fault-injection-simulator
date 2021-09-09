8d7ad73759f285e1819b51b811ec298bc2b5b752
Simone Alberto Peirone <s286886@studenti.polito.it>
2021-09-09 10:38:37 +0200

    Fix pthread_create arguments

--- simulator/Posix/thread.c
+++ simulator/Posix/thread.c
@@ -8,23 +8,34 @@
 
 #include "../thread.h"
 
-int launchThread(void* (*function) (void*),
-                const void * address,
-                const unsigned long injTime,
-                const unsigned long timeoutNs,
-                const unsigned long offsetByte,
-                const unsigned long offsetBit,
-                thread_t * id){
+int launchThread(void *(*function)(void *),
+                 const void *address,
+                 const unsigned long injTime,
+                 const unsigned long timeoutNs,
+                 const unsigned long offsetByte,
+                 const unsigned long offsetBit,
+                 thread_t *id)
+{
     pthread_t thread_id;
-    thData_t data = (thData_t) {address, injTime, timeoutNs, offsetByte, offsetBit};
-    if(pthread_create(&thread_id, NULL, function, (void *)&data) != 0){
+
+    // TODO: at the moment there is NOT a corresponding free operation
+    thData_t *data = (thData_t *)malloc(sizeof(thData_t));
+    data->address = address;
+    data->injTime = injTime;
+    data->timeoutNs = timeoutNs;
+    data->offsetByte = offsetByte;
+    data->offsetBit = offsetBit;
+    
+    if (pthread_create(&thread_id, NULL, function, (void *)data) != 0)
+    {
         return INJECTOR_THREAD_FAILURE;
     }
     id->thread_id = thread_id;
     return INJECTOR_THREAD_SUCCESS;
 }
 
-int detachThread(thread_t * id){
+int detachThread(thread_t *id)
+{
     pthread_detach(id->thread_id);
     return INJECTOR_THREAD_SUCCESS;
 }
