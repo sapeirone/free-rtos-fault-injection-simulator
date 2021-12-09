43244cae5c169762c1698450da8fc714eefa1527
Simone Alberto Peirone <simone.peirone.98@gmail.com>
2021-12-09 10:36:32 +0100

    Fix win32 compilation script

--- run_win32.bat
+++ run_win32.bat
@@ -1,10 +1,49 @@
-ECHO "Building for Win32 target..."
+echo off
+
+echo Free RTOS injection simulator: building for Win32 target...
+
+@rem Despite /r "Processes search strings as regular expressions" findstr ignores the regex
+for /f "delims=="   %%l in ('cmake --help ^| findstr /r "Visual Studio \d+ \d{4}\s+(?![arch])="') do (
+  echo "%%l" | find "arch">nul
+  if errorlevel 1 (
+      set generator=%%l
+      goto :build
+  )
+)
+
+if "%generator%"=="" (
+    echo No suitable cmake generator found! Aborting...
+    exit /b 1
+)
+
+:build
+
+set generator=%generator:~2%
+
+@rem i don't know why the following code works and i'm too afraid to ask
+@rem (source: https://www.dostips.com/DtTipsStringManipulation.php#Snippets.TrimRightFOR)
+set generator=%generator%##
+set generator=%generator:                ##=##%
+set generator=%generator:        ##=##%
+set generator=%generator:    ##=##%
+set generator=%generator:  ##=##%
+set generator=%generator: ##=##%
+set generator=%generator:##=%
+
+echo Selected generator is "%generator%"
 
 del /s CMakeCache.txt >nul
-cmake . -G "Visual Studio 17" -A Win32
-msbuild.exe ALL_BUILD.vcxproj
 
-copy build\Debug\sim.exe sim.exe
+echo Compiling...
 
-ECHO "Done!"
+cmake . -G "%generator%" -A Win32 > cmake_build.log
+msbuild.exe ALL_BUILD.vcxproj > msbuild.log
 
+set BUILD_STATUS=%ERRORLEVEL%
+if %BUILD_STATUS%==0 (
+    move build\Debug\sim.exe sim.exe > nul
+    echo Done! You can run the simulator with ./sim.exe
+    exit /b 0
+) 
+
+echo Compilation error! Check cmake_build.log and msbuild.log
