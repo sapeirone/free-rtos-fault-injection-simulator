77bf7f5b09218e58abbcfd3dae52923bc1f09e74
Simone Alberto Peirone <simone.peirone.98@gmail.com>
2021-12-09 12:08:22 +0100

    Fix Posix compilation script

--- run_posix.sh
+++ run_posix.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 
-echo "Building Posix target..."
+echo "Free RTOS injection simulator: building for Posix target..."
 
 rm -f CMakeCache.txt
-cmake .
-make
 
-echo "Done!"
+echo "Compiling..."
+
+cmake . &> cmake_build.log
+if [[ $? -ne 0 ]]; then
+    echo "cmake error! Check cmake_build.log"
+    exit 1
+fi
+
+make &> make_build.log
+if [[ $? -ne 0 ]]; then
+    echo "make compilation error! Check cmake_build.log"
+    exit 2
+fi
+
+cp build/sim sim.out
+
+echo "Done! You can run the simulator with ./sim.out"
