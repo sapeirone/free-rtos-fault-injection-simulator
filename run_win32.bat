bf949801db63abce34c865fce1d32305e8fb4192
Simone Alberto Peirone <simone.peirone.98@gmail.com>
2021-12-16 11:47:49 +0100

    Fix the generator selection in the win32 build script

--- run_win32.bat
+++ run_win32.bat
@@ -1,13 +1,21 @@
 echo off
+Setlocal EnableDelayedExpansion
 
 echo Free RTOS injection simulator: building for Win32 target...
 
 @rem Despite /r "Processes search strings as regular expressions" findstr ignores the regex
+
+set generator=null
 for /f "delims=="   %%l in ('cmake --help ^| findstr /r "Visual Studio \d+ \d{4}\s+(?![arch])="') do (
   echo "%%l" | find "arch">nul
   if errorlevel 1 (
+    if !generator!==null (
       set generator=%%l
-      goto :build
+    ) else (
+      if "%l:~0,1%"=="*" (
+        set generator=%%l
+      )
+    )
   )
 )
 
@@ -15,8 +23,6 @@ if "%generator%"=="" (
     echo No suitable cmake generator found! Aborting...
     exit /b 1
 )
-
-:build
 
 set generator=%generator:~2%
 
@@ -32,12 +38,12 @@ set generator=%generator:##=%
 
 echo Selected generator is "%generator%"
 
-del /s CMakeCache.txt >nul
+del /s CMakeCache.txt > nul 2>&1
 
 echo Compiling...
 
-cmake . -G "%generator%" -A Win32 > cmake_build.log
-msbuild.exe ALL_BUILD.vcxproj > msbuild.log
+cmake . -G "%generator%" -A Win32 > cmake_build.log 2>&1
+msbuild.exe ALL_BUILD.vcxproj > msbuild.log 2>&1
 
 set BUILD_STATUS=%ERRORLEVEL%
 if %BUILD_STATUS%==0 (
